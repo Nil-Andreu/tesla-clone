d2a196f2b0a070a08c196462cfcb9b7c20899a58
Nil Andreu <nilandreug@gmail.com>
2021-05-20 20:06:52 +0200

    Firebase configuration

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAwwVpzfBufq5PaaEcBdm3XMX7MnRHwO7k",
+    authDomain: "teslaclone-f4a70.firebaseapp.com",
+    projectId: "teslaclone-f4a70",
+    storageBucket: "teslaclone-f4a70.appspot.com",
+    messagingSenderId: "268455991909",
+    appId: "1:268455991909:web:8d003a4fa2a97d67f3c48c",
+    measurementId: "G-9KYKCRZMWZ"
+  };
