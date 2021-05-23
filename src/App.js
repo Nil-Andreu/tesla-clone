e8e59b01ca9fd0d89fb917e5531afb572fb07721
Nil Andreu <nilandreug@gmail.com>
2021-05-23 21:41:07 +0200

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './components/Header.jsx';
-import Home from './components/Home.jsx';
-import Sidebar from './components/Sidebar/Sidebar.jsx'
-
-import {
-  BrowserRouter as Router, Route, Switch
-} from 'react-router-dom'
 
 function App() {
-  //Create a state of the sidebar. By default, this state is false
-  const [isOpen, setIsOpen] = useState(false)
-
-  // On the click, gonna change the state
-  const toggle = () => {
-    //Change the state of the openned
-    setIsOpen(!isOpen)
-  }
 
   return (
     <div className="App">
-      <Router>
-        <Header toggle={toggle}/>
-        <Sidebar isOpen={isOpen} toggle={toggle}/>
-        
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route exact path="/login/">
-          </Route>
-        </Switch>
-
-      </Router>
+      <h1>Hello world</h1>
     </div>
     
   );
